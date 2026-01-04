aca5e251cd62db4746c1be9f46c565ffe1c47c48
tukku <cksinha844115@gmail.com>
2026-01-04 21:45:08 +0530

    news summary functions are implemented

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer'
 import { WELCOME_EMAIL_TEMPLATE } from './templates'
+import { NEWS_SUMMARY_EMAIL_PROMPT } from '../inngest/prompts'
 
 export const transporter = nodemailer.createTransport({
     service:'gmail',
@@ -16,10 +17,26 @@ export const sendWelcomeEmail = async ({email,name,intro}:WelcomeEmailData)=>{
     
 
     const mailOptions = {
-        from: `"Hotstock <cksinha844115@gmail.com>"`,
+        from: `"Signalist <cksinha844115@gmail.com>"`,
         to:email,
         subject:`Welcome to signalist - your stock market toolkit is ready!`,
         text: 'Thanks for joining Signalist',
+        html: htmlTemplate,
+    }
+
+    await transporter.sendMail(mailOptions)
+}
+
+export const sendNewsSummaryEmail = async({email,date,newsContent}:{email:string;date:string;newsContent:string;}):Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_PROMPT
+        .replace('{{date}}',date)
+        .replace('{{newsContent}}',newsContent)
+
+    const mailOptions = {
+        from: `"Signalist News<cksinha844115@gmail.com>"`,
+        to: email,
+        subject:`📈 Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Signalist`,
         html: htmlTemplate,
     }
 
